88d2b27e98f69cf18d890708efd589de2cc47092
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-11-15 17:54:45 -0300

    feat: improve seed generation

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -2,14 +2,20 @@
  * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
  */
 import { createSeedClient } from '@snaplet/seed'
-import { createUser } from '../factories/user-factory'
+import { makeUser } from '../factories/user-factory'
+import { makeCheatSheet } from '../factories/cheat-sheet-factory'
 
 const main = async () => {
   const seed = await createSeedClient()
 
   await seed.$resetDatabase()
 
-  await seed.user((x) => x(1000, ({ index }) => createUser({ id: index })))
+  await seed.user((x) =>
+    x({ min: 0, max: 15 }, () => ({
+      ...makeUser(),
+      cheatSheet: (x) => x({ min: 0, max: 20 }, () => makeCheatSheet()),
+    })),
+  )
 
   console.log('Database seeded successfully!')
 
