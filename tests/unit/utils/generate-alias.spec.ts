71f48fb421affdb0d66ccf56f2e948c16d406a17
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-11-16 12:40:18 -0300

    test: improve generateAlias tests

--- tests/unit/utils/generate-alias.spec.ts
+++ tests/unit/utils/generate-alias.spec.ts
@@ -3,25 +3,34 @@ import { generateAlias } from '~/server/utils/generate-alias'
 import { hasSpecialChars } from '~/server/utils/has-special-chars'
 
 describe('Aliases generation', () => {
-  it('should return the first 3 chars in lowercase', () => {
+  it('returns the first 3 chars in lowercase', () => {
     expect(generateAlias('Hello World')).to.be.equal('hel')
   })
 
-  it('should be less than or equal 3 chars', () => {
+  it('is less than or equal 3 chars', () => {
     expect(generateAlias('hello world').length).to.be.lessThanOrEqual(3)
   })
 
-  it('should contain a suffix', () => {
+  it('contains a suffix', () => {
     expect(
       generateAlias('hello world', { suffix: '123' }).endsWith('123'),
     ).toBeTruthy()
   })
 
-  it('should strip special chars', () => {
+  it('generates alias for less than 3 chars string', () => {
+    expect(generateAlias('he')).to.be.equal('he')
+    expect(generateAlias('h')).to.be.equal('h')
+  })
+
+  it('contains a suffix for less than 3 chars string', () => {
+    expect(generateAlias('he', { suffix: '123' }))
+  })
+
+  it('strip special chars', () => {
     expect(hasSpecialChars(generateAlias('!Hello World'))).toBeFalsy()
   })
 
-  it('should be empty for empty string', () => {
-    expect(generateAlias('')).to.be.equals('')
+  it('is empty for empty string', () => {
+    expect(generateAlias('')).equals('')
   })
 })
