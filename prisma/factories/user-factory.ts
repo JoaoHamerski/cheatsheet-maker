20a65f850427ab2c6e12a3c86190858942fa1108
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-11-16 12:47:35 -0300

    feat: extract makeUser function signature to type

--- prisma/factories/user-factory.ts
+++ prisma/factories/user-factory.ts
@@ -2,7 +2,9 @@ import type { User } from '@prisma/client'
 import { faker } from '@faker-js/faker'
 import { generateSyncCode } from '~/server/utils/generate-sync-code'
 
-export const makeUser = (overwrites: Partial<User> = {}): Partial<User> => {
+type MakeUserFn = (overwrites?: Partial<User>) => User
+
+export const makeUser: MakeUserFn = (overwrites = {}) => {
   const timestamp = faker.date.past()
   const fullName = faker.person.firstName() + ' ' + faker.person.lastName()
 
@@ -13,5 +15,5 @@ export const makeUser = (overwrites: Partial<User> = {}): Partial<User> => {
     createdAt: timestamp,
     updatedAt: timestamp,
     ...overwrites,
-  }
+  } as User
 }
