02b89b939ce57b3abe6d214a312d856f36d94dd3
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-11-16 12:52:49 -0300

    refactor: improve cheat-sheet-factory code

--- prisma/factories/cheat-sheet-factory.ts
+++ prisma/factories/cheat-sheet-factory.ts
@@ -3,15 +3,18 @@ import type { CheatSheet } from '@prisma/client'
 import { upperFirst } from 'lodash-es'
 import { generateAlias } from '~/server/utils/generate-alias'
 
-export const makeCheatSheet = (
-  overwrites: Partial<CheatSheet> = {},
-): Partial<CheatSheet> => {
+type MakeCheatSheetFn = (overwrites?: Partial<CheatSheet>) => CheatSheet
+
+export const makeCheatSheet: MakeCheatSheetFn = (
+  overwrites = {},
+): CheatSheet => {
   const title = faker.lorem.words({ min: 2, max: 10 })
   const uuid = faker.string.uuid()
+
   return {
     title: upperFirst(title),
     uuid,
     alias: generateAlias(title, { suffix: uuid.slice(0, 3) }),
     ...overwrites,
-  }
+  } as CheatSheet
 }
