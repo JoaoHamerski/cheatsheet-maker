436dd27137e69613af5862c0d592d2dfa7f27edf
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-11-20 13:14:40 -0300

    feat: call new seeders on main seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,15 @@
+import { cheatSheetItemSeeder } from './seeders/cheat-sheet-item-seeder'
 import { cheatSheetSeeder } from './seeders/cheat-sheet-seeder'
+import { tagSeeder } from './seeders/tag-seeder'
 import { userSeeder } from './seeders/user-seeder'
 
 async function main() {
-  const SEEDERS = [userSeeder, cheatSheetSeeder]
+  const SEEDERS = [
+    tagSeeder,
+    userSeeder,
+    cheatSheetSeeder,
+    cheatSheetItemSeeder,
+  ]
 
   for (const seed of SEEDERS) {
     await seed()
