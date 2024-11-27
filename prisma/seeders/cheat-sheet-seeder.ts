cd2dae6240866ada0122a3d5b94c7f36536e73fc
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-11-26 21:06:30 -0300

    refactor: cheat sheet seeder

--- prisma/seeders/cheat-sheet-seeder.ts
+++ prisma/seeders/cheat-sheet-seeder.ts
@@ -1,25 +1,23 @@
 import { faker } from '@faker-js/faker'
-import { map, sortBy, times } from 'lodash-es'
+import { flatten, times } from 'lodash-es'
 import prisma from '~/lib/prisma'
 import { makeCheatSheet } from '../factories/cheat-sheet-factory'
+import { getUserIds } from '../utils'
 
 export const cheatSheetSeeder = async () => {
-  const users = await prisma.user.findMany({ select: { id: true } })
-  const userIds = sortBy(map(users, 'id'))
-
-  const cheatSheets = []
-
-  for (const userId of userIds) {
-    const CHEAT_SHEETS_PER_USER = faker.number.int({ min: 0, max: 15 })
-
-    const generatedCheatSheets = times(CHEAT_SHEETS_PER_USER, () =>
-      makeCheatSheet({ userId }),
-    )
-
-    cheatSheets.push(...generatedCheatSheets)
-  }
-
   await prisma.cheatSheet.createMany({
-    data: cheatSheets,
+    data: await generateCheatSheets(),
   })
+}
+
+const generateCheatSheets = async () => {
+  const userIds = await getUserIds()
+
+  return flatten(userIds.map((userId) => generateUserCheatSheets(userId)))
+}
+
+const generateUserCheatSheets = (userId: number) => {
+  const CHEAT_SHEETS_PER_USER = faker.number.int({ min: 0, max: 15 })
+
+  return times(CHEAT_SHEETS_PER_USER, () => makeCheatSheet({ userId }))
 }
