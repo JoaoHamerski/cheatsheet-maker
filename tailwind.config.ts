6c84d9c036c6b3b517e5e3a0ab78bdcfd47327c0
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-21 18:34:59 -0300

    feat: add custom daisyui colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,21 @@
 import type { Config } from 'tailwindcss'
 import daisyUI from 'daisyui'
+import daisyUIThemes from 'daisyui/src/theming/themes'
 
 export default <Partial<Config>>{
   plugins: [daisyUI],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...daisyUIThemes['dark'],
+          primary: '#0070E8',
+          secondary: '#6A4C93',
+          warning: '#FFCA3A',
+          error: '#ff595e',
+          success: '#8AC926',
+        },
+      },
+    ],
+  },
 }
