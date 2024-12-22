99f08c3578ba527a0ff18b43c0edcff37231e735
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-21 21:07:18 -0300

    feat: add base cheat sheet card component

---
+++ components/cheat-sheet/CheatSheetCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { CheatSheetItem } from '@prisma/client'
+
+defineProps<{
+  cheatSheetItem: CheatSheetItem
+}>()
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center p-4 text-xl font-bold text-center text-gray-900 rounded-lg shadow-lg bg-primary"
+  >
+    {{ cheatSheetItem.title }}
+  </div>
+</template>
