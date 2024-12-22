a1673042a02a733d48f023bdfc760a8bd26bf3f5
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-22 03:38:34 -0300

    feat: add badge component

---
+++ components/app/AppBadge.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Icon } from '#build/components'
+
+defineProps<{
+  icon?: InstanceType<typeof Icon>['$props']
+  hasIcon?: boolean
+}>()
+</script>
+
+<template>
+  <span
+    class="rounded-full text-sm px-2 font-bold"
+    :class="{
+      'flex items-center w-fit gap-2': !!icon,
+    }"
+  >
+    <Icon
+      v-if="icon"
+      v-bind="icon"
+    />
+    <slot />
+  </span>
+</template>
