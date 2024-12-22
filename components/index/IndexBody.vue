1a61d581a1040d684de0dab7ea3cbbfc6e7f44bf
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-21 21:07:10 -0300

    feat: add index body

---
+++ components/index/IndexBody.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { data: items } = await useFetch('/api/cheat-sheet-items')
+</script>
+
+<template>
+  <div class="grid grid-cols-5 gap-3">
+    <CheatSheetCard
+      v-for="item in items"
+      :key="item.id"
+      :cheat-sheet-item="item"
+    />
+  </div>
+</template>
