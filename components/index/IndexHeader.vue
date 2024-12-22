a03a7f106fa3a37fef251af5183e2c83b94eef73
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-21 21:06:02 -0300

    feat: add index header related base component

--- components/index/IndexHeader.vue
+++ components/index/IndexHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <CheatSheetItem></CheatSheetItem>
+  <div class="flex justify-between my-16 relative h-14">
+    <IndexHeaderNewBtn />
+    <IndexHeaderSearch class="w-1/2 absolute left-1/2 -translate-x-1/2" />
+    <div />
   </div>
 </template>
---
+++ components/index/IndexHeaderNewBtn.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="flex items-center gap-2 px-6 font-bold text-lg uppercase rounded-lg bg-gradient-to-r from-primary to-blue-500 hover:scale-[1.02] transition-all active:scale-100"
+  >
+    <Icon
+      name="ph:plus-circle-fill"
+      size="1.5rem"
+    ></Icon>
+    New sheet
+  </button>
+</template>
