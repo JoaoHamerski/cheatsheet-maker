c02c44ef49ae64882737916b8914c545f569a927
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-24 05:48:54 -0300

    feat: show modal on new sheet button click

--- components/index/IndexHeaderNewBtn.vue
+++ components/index/IndexHeaderNewBtn.vue
@@ -1,11 +1,19 @@
+<script setup lang="ts">
+const modalOpen = ref(false)
+</script>
+
 <template>
-  <button
-    class="flex items-center gap-2 px-6 font-bold text-lg uppercase rounded-lg bg-gradient-to-r from-primary to-blue-500 hover:scale-[1.02] transition-all active:scale-100"
-  >
-    <Icon
-      name="ph:plus-circle-fill"
-      size="1.5rem"
-    ></Icon>
-    New sheet
-  </button>
+  <div class="flex">
+    <button
+      class="flex items-center gap-2 px-6 font-bold text-lg uppercase rounded-lg bg-gradient-to-r from-primary to-blue-500 hover:scale-[1.02] transition-all active:scale-100"
+      @click="modalOpen = true"
+    >
+      <Icon
+        name="ph:plus-circle-fill"
+        size="1.5rem"
+      />
+      New sheet
+    </button>
+    <IndexHeaderNewBtnModal v-model="modalOpen" />
+  </div>
 </template>
---
+++ components/index/IndexHeaderNewBtnModal.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const open = ref(false)
+</script>
+
+<template>
+  <AppModal v-model="open">
+    <template #body> Olá mundo </template>
+  </AppModal>
+</template>
