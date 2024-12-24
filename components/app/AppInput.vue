2247442b414ad759011e172a21dd2cac5bf11ca8
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-24 08:15:54 -0300

    feat: add new input component

---
+++ components/app/AppInput.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import type { Icon } from '#build/components'
+
+interface AppInputProps {
+  id?: string
+  name: string
+  label?: string
+  placeholder?: string
+  icon?: InstanceType<typeof Icon>['$props']
+  inputClass?: string
+}
+
+const modelValue = defineModel<string | number>()
+
+const emit = defineEmits(['update:modelValue'])
+const props = withDefaults(defineProps<AppInputProps>(), {
+  id: '',
+  label: '',
+  placeholder: '',
+  icon: undefined,
+  inputClass: '',
+  kbd: undefined,
+})
+
+const inputId = computed(() => props.name ?? props.id)
+
+const onInput = (event: Event) => {
+  const element = event.target as HTMLInputElement
+
+  emit('update:modelValue', element.value)
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <label
+      :for="name"
+      class="text-gray-300 self inline font-semibold w-fit"
+      >{{ label }}</label
+    >
+
+    <label
+      :for="name"
+      class="bg-[rgb(var(--input-bg))] rounded-md w-full px-4 py-3 focus-within:outline focus-within:outline-3 focus-within:outline-primary text-gray-300 flex items-center gap-2"
+    >
+      <Icon
+        v-if="icon"
+        v-bind="icon"
+      />
+      <input
+        :id="inputId"
+        type="text"
+        class="bg-transparent text-gray-300 focus:outline-none w-full"
+        :class="inputClass"
+        :placeholder="placeholder"
+        :name="name"
+        :value="modelValue"
+        @input="onInput"
+      />
+      <div v-if="$slots['append']">
+        <slot name="append"></slot>
+      </div>
+    </label>
+  </div>
+</template>
