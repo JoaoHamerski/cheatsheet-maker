83b67d6556550fae44bcb852e7f9ac4a86db0fb4
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-24 11:40:11 -0300

    feat: add new sheet modal body

--- components/index/IndexHeaderNewBtnModal.vue
+++ components/index/IndexHeaderNewBtnModal.vue
@@ -5,11 +5,30 @@ const open = ref(false)
 <template>
   <AppModal v-model="open">
     <template #body>
-      <AppInput
-        label="E-mail"
-        name="email"
-        placeholder="Digite seu email..."
-      />
+      <form action="#">
+        <div class="flex flex-col gap-12 mt-5">
+          <div class="text-center text-gray-300">
+            <Icon
+              name="ph:scroll-fill"
+              size="4rem"
+            />
+          </div>
+          <AppInput
+            name="sheet-name"
+            placeholder="Type the sheet name..."
+            input-class="text-center h-10 text-xl"
+            :pt="{ tabindex: 1 }"
+          />
+          <AppButton
+            label="CREATE"
+            block
+            color="success"
+            class="font-bold py-3"
+            animated
+            type="submit"
+          />
+        </div>
+      </form>
     </template>
   </AppModal>
 </template>
