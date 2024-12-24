237ab05bb4b24aa2d56fd896719f62a1fd038928
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-24 08:08:16 -0300

    feat: improve no items found message

--- components/cheat-sheet/CheatSheetCardBodyEmpty.vue
+++ components/cheat-sheet/CheatSheetCardBodyEmpty.vue
@@ -1,12 +1,17 @@
 <template>
-  <a
-    href=""
-    class="flex flex-col gap-3 items-center justify-center h-full bg-red text-blue-300 hover:text-blue-400 transition-colors"
-  >
-    <Icon
-      name="ph:plus-circle-fill"
-      size="3rem"
-    ></Icon>
-    <span class="font-bold"> NEW ITEM </span>
-  </a>
+  <div class="flex flex-col items-center justify-center h-full">
+    <a
+      href=""
+      class="flex flex-col gap-3 items-center justify-center h-full bg-red hover:text-white transition-colors"
+    >
+      <Icon
+        name="ph:plus-circle-fill"
+        size="3rem"
+      />
+      <span class="font-bold"> NEW ITEM </span>
+    </a>
+    <div class="text-xs font-semibold uppercase text-gray-500">
+      No items found
+    </div>
+  </div>
 </template>
