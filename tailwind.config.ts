a8b2899e823648e2a269f60024ff1cc29c13ad5f
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-24 07:37:02 -0300

    refactor: rework color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
 
 export default <Partial<Config>>{
   theme: {
-    colors: {
-      ...colors,
-      'primary': '#0070E8',
-      'secondary': '#00E8C9',
-      'warning': '#FFCA3A',
-      'danger': '#ff595e',
-      'success': '#24E16D',
-      'base-100': '#1d232a',
-      'base-200': '#191e24',
-      'base-300': '#15191e',
+    extend: {
+      colors: {
+        'primary': 'rgb(var(--primary))',
+        'secondary': 'rgb(var(--secondary))',
+        'success': 'rgb(var(--success))',
+        'warning': 'rgb(var(--warning))',
+        'danger': 'rgb(var(--danger))',
+        'base-100': 'rgb(var(--base-100))',
+        'base-200': 'rgb(var(--base-200))',
+        'base-300': 'rgb(var(--base-300))',
+      },
     },
   },
 }
