c03ab534f1bf77cf6331e202aa5bb4d21e22f5e1
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-26 11:05:15 -0300

    feat: add cog icon button on header

--- components/index/IndexHeader.vue
+++ components/index/IndexHeader.vue
@@ -2,6 +2,14 @@
   <div class="flex justify-between my-16 relative h-14">
     <IndexHeaderNewBtn />
     <IndexHeaderSearch class="w-1/2 absolute left-1/2 -translate-x-1/2" />
-    <div />
+    <AppButton
+      v-tippy="{
+        content: 'Settings',
+        placement: 'bottom',
+      }"
+      color="gray"
+      :icon="{ name: 'ph:gear-six', size: '1.35rem' }"
+      variant="text"
+    />
   </div>
 </template>
