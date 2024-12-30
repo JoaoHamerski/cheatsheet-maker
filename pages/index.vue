9aa76dbefa60fa10b2bdf97a6edf8a17c8f39c8c
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-30 16:38:04 -0300

    feat: add type check for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-const { data: cheatSheets } = await useFetch('/api/cheat-sheets')
+import type { CheatSheetsIndex } from '~/server/routes/api/cheat-sheets/index.get'
+
+const { data: cheatSheets } =
+  await useFetch<CheatSheetsIndex>('/api/cheat-sheets')
 </script>
 
 <template>
   <div class="container mx-auto">
     <IndexHeader />
-    <IndexBody :cheat-sheets="cheatSheets" />
+    <IndexBody
+      v-if="cheatSheets"
+      :cheat-sheets="cheatSheets"
+    />
   </div>
 </template>
