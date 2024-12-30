b5fbd825b2bc835d6eb4cc1ea77734d09ff2bede
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-12-30 16:38:18 -0300

    feat: add basic error page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <AppButton
+      label="Go back to home"
+      class="font-bold"
+      animated
+    ></AppButton>
+  </div>
+</template>
